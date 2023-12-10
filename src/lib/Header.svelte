70e7fa33fdd68d51c16c228617a30e14e8d90c72
ungnome <73322863+ungnome@users.noreply.github.com>
2023-12-10 11:01:39 -0500

    core(Header): move styles from inline to <style> tag

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,18 +2,28 @@
 	import { search, total } from './tabs';
 </script>
 
-<header
-	class="sticky top-0 z-50 w-screen flex flex-col content-center drp-shadow-sm bg-white dark:bg-neutral-800 dark:text-white">
-	<h2 class="m-3 text-2xl font-semibold">Tabitat</h2>
-
+<header id="header">
+	<h2 id="title">Tabitat</h2>
 	<input
+		id="search-input"
 		type="search"
 		placeholder="Search"
 		bind:value={$search}
-		autofocus
-		class="mx-4 mb-4 p-1 border border-gray-300 rounded-lg text-base focus:outline-none focus:border-2 focus:border-indigo-400 focus:ring-indigo-200 dark:bg-neutral-800" />
+		autofocus />
 
 	<p class="self-center mb-1 text-base">Total Tabs: {$total}</p>
 </header>
 
-<style></style>
+<style lang="postcss">
+	#header {
+		@apply sticky top-0 z-50 w-screen flex flex-col content-center drop-shadow-sm bg-white dark:bg-neutral-800 dark:text-white;
+	}
+
+	#title {
+		@apply m-3 text-2xl font-semibold;
+	}
+
+	#search-input {
+		@apply mx-4 mb-4 p-1 border border-gray-300 rounded-lg text-base focus:outline-none focus:border-2 focus:border-indigo-400 focus:ring-indigo-200 dark:bg-neutral-800;
+	}
+</style>
